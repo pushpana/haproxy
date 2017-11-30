f51d164b7b464f32a9a94eac3737cc30f2f292e1
pushpana <p.aishwariya@gmail.com>
2017-11-30 15:01:06 -0600

    Initial commit

--- recipes/default.rb
+++ recipes/default.rb
@@ -3,3 +3,10 @@
 # Recipe:: default
 #
 # Copyright:: 2017, The Authors, All Rights Reserved.
+node.default['haproxy']['members'] = [{
+    'hostname' => 'ec2-54-211-81-89.compute-1.amazonaws.com',
+    'ipaddress' => '54.211.81.89',
+    'port' => 80,
+    'ssl_port' => 80
+}]
+include_recipe 'haproxy::manual'
